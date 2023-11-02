04e02f4cff52a0a0109d026d91846e1121724c42
DELL <manasajalagam379@gmail.com>
2023-11-02 20:51:08 +0530

    expense-shell

---
+++ backend.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+cp backend.service /etc/systemd/system/backend.service
+
+dnf install nodejs -y
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+npm install
+systemctl daemon-reload
+
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
+mysql -h <MYSQL-54.198.99.22 -uroot -pExpenseApp@1 < /app/schema/backend.sql
